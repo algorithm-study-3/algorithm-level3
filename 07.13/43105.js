72f57595b364fc0eda62c337bfe595fde5448701
jin123457 <jinsol9909@gmail.com>
2025-07-13 03:09:05 +0900

    feat : ✅ DP 문제 구현 (3/3)

---
+++ 07.13/43105.js
@@ -0,0 +1,13 @@
+function solution(triangle) {
+  const N = triangle.length;
+  const dp = Array.from({ length: N }, () => Array(N).fill(0));
+  dp[N - 1] = triangle[N - 1];
+  for (let i = N - 2; i >= 0; i--) {
+    for (let j = 0; j < triangle[i].length; j++) {
+      dp[i][j] = Math.max(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j];
+    }
+  }
+  return dp[0][0];
+}
+
+solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]);
