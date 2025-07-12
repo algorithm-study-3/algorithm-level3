35cdf9ae6669742a80acfc3390bda19a3e1ba1cd
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-07-12 16:38:34 +0900

    정수 삼각형: dp

---
+++ 07/12/정수삼각형.js
@@ -0,0 +1,17 @@
+function solution(triangle) {
+  var answer = 0
+  const dp = Array.from({ length: triangle.length }, (_, idx) =>
+    Array.from({ length: idx + 1 }, () => 0),
+  )
+
+  dp[0][0] = triangle[0][0]
+  for (let i = 1; i < triangle.length; i += 1) {
+    for (let j = 0; j < triangle[i].length; j += 1) {
+      const cur = triangle[i][j]
+      dp[i][j] = Math.max(dp[i - 1][j - 1] ?? 0, dp[i - 1][j] ?? 0) + cur
+      answer = Math.max(answer, dp[i][j])
+    }
+  }
+
+  return answer
+}
