5fd6e6f6073767d5e6a21a6a2eb2b549eb705086
hoyyChoi <hoyeon8165@gmail.com>
2025-08-09 23:59:42 +0900

    fix: toss 2번 문제 수정

--- 프로그래머스/toss/second.js
+++ 프로그래머스/toss/second.js
@@ -1,19 +1,13 @@
 function setDeep(obj, path, value) {
   const keys = path.split(".");
-  let current = obj;
+  let newObj = obj;
 
   for (let i = 0; i < keys.length; i++) {
-    const key = keys[i];
-
-    if (i === keys.length - 1) {
-      // 마지막 키면 값 할당
-      current[key] = value;
+    if (i !== keys.length - 1) {
+      newObj[keys[i]] = {};
+      newObj = newObj[keys[i]];
     } else {
-      // 다음 단계가 없거나 객체가 아니면 빈 객체로 생성
-      if (typeof current[key] !== "object" || current[key] === null) {
-        current[key] = {};
-      }
-      current = current[key];
+      newObj[keys[i]] = value;
     }
   }
 
